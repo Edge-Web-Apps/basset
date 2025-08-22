23f8a05b6b99e16fa1cb2231378e0fba8f14654e
Bri MacBain <35091265+brimacbain@users.noreply.github.com>
2025-08-22 11:52:48 -0700

    Update functions.php

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -3,16 +3,17 @@
 use Backpack\Basset\Enums\StatusEnum;
 use Backpack\Basset\Facades\Basset;
 
-if (! function_exists('basset')) {
-    function basset(string $asset): string
-    {
-        // cache the asset without output
-        $status = Basset::basset($asset, false);
-
-        if (in_array($status, [StatusEnum::DISABLED, StatusEnum::INVALID])) {
-            return $asset;
-        }
-
-        return Basset::getUrl($asset);
-    }
-}
+//Disable function
+//if (! function_exists('basset')) {
+//    function basset(string $asset): string
+//    {
+//        // cache the asset without output
+//        $status = Basset::basset($asset, false);
+//
+//        if (in_array($status, [StatusEnum::DISABLED, StatusEnum::INVALID])) {
+//            return $asset;
+//        }
+//
+//        return Basset::getUrl($asset);
+//    }
+//}
