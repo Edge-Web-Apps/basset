0e78e3e15e886ab33c4c049d6ad8f72422e03f85
Bri MacBain <35091265+brimacbain@users.noreply.github.com>
2025-08-23 20:30:55 -0700

    Update BassetServiceProvider.php

--- src/BassetServiceProvider.php
+++ src/BassetServiceProvider.php
@@ -87,63 +87,7 @@ class BassetServiceProvider extends ServiceProvider
     {
         $this->callAfterResolving('blade.compiler', function (BladeCompiler $bladeCompiler) {
             // Basset
-            $bladeCompiler->directive('basset', function (string $parameter): string {
-                return asset('packages/'.Basset::getPath($parameter));
-/*                return "<?php Basset::basset({$parameter}); ?>";*/
-            });
-
-            // Basset Directory
-            $bladeCompiler->directive('bassetDirectory', function (string $parameter): string {
-                return "<?php Basset::bassetDirectory({$parameter}); ?>";
-            });
-
-            // Basset Archive
-            $bladeCompiler->directive('bassetArchive', function (string $parameter): string {
-                return "<?php Basset::bassetArchive({$parameter}); ?>";
-            });
-
-            // Basset Code Block
-            $bladeCompiler->directive('bassetBlock', function (string $parameter): string {
-                return "<?php \$bassetBlock = {$parameter}; ob_start(); ?>";
-            });
-
-            $bladeCompiler->directive('endBassetBlock', function (): string {
-                return '<?php Basset::bassetBlock($bassetBlock, ob_get_clean()); ?>';
-            });
-
-            // DEPRECATED - Please use `@basset` or `@bassetBlock`. @loadOnce Will be completely removed in Backpack v7.
-            $bladeCompiler->directive('loadOnce', function (string $parameter): string {
-                // determine if it's a CSS or JS file
-                $cleanParameter = Str::of($parameter)->trim("'")->trim('"')->trim('`');
-                $filePath = Str::of($cleanParameter)->before('?')->before('#');
-
-                if (Str::endsWith($filePath, ['.js', '.css'])) {
-                    return asset('packages/'.Basset::getPath($parameter));
-/*                    return "<?php Basset::basset({$parameter}); ?>";*/
-                }
-
-                // in case it's not js or css specifically, we assume it's a block of javascript code.
-                // this is to keep backward compatibility with our previous usage of @loadOnce to load blocks of JS.
-                // we never used it for css and the recommended way was always to @loadOnce(file.css) or @loadOnce(file.js)
-                // this is a temporary solution until we remove @loadOnce completely.
-                $filePath = Str::of($filePath)->finish('.js')->value();
-
-                return "<?php \$bassetBlock = '{$filePath}'; ob_start(); ?>";
-            });
-
-            $bladeCompiler->directive('endLoadOnce', function (): string {
-                return '<?php Basset::bassetBlock($bassetBlock, ob_get_clean(), true, false); ?>';
-            });
-
-            $bladeCompiler->directive('loadStyleOnce', function (string $parameter): string {
-                return asset('packages/'.Basset::getPath($parameter));
-/*                return "<?php Basset::basset({$parameter}); ?>";*/
-            });
-
-            $bladeCompiler->directive('loadScriptOnce', function (string $parameter): string {
-                return asset('packages/'.Basset::getPath($parameter));
-/*                return "<?php Basset::basset({$parameter}); ?>";*/
-            });
+            return true;
         });
     }
 
